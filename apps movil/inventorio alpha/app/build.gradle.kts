b70085352d4c656bbfecd8879bf47d1793b20413
Angel <angel@example.com>
2026-07-16 03:41:10 -0600

    chore: OTA Update Alpha v2.0.49 (Code 63)

--- apps movil/inventorio alpha/app/build.gradle.kts
+++ apps movil/inventorio alpha/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.inventorio.alpha"
     minSdk = 24
     targetSdk = 36
-    versionCode = 62
-    versionName = "2.0.48"
+    versionCode = 63
+    versionName = "2.0.49"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     ndk {
