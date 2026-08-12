004556f7b8fa55baefd60989d1899f783d226d9e
Angel <angel@example.com>
2026-08-12 05:35:15 -0600

    chore: OTA Update AURA Club v1.0.5 (Code 6)

--- apps movil/inventorio loyalty/app/build.gradle.kts
+++ apps movil/inventorio loyalty/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.inventorio.loyalty"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "1.0.3"
+    versionCode = 6
+    versionName = "1.0.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
